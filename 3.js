4b23101ce9ac6ff0628703750d260cfbe7e022c1
Hyerin Kim <kimh764@newschool.edu>
2023-11-26 17:59:16 -0500

    fix

--- 3.js
+++ 3.js
@@ -1,32 +1,43 @@
-let x = 100,
-  y = 100,
-  angle1 = 0.0,
-  segLength = 50;
+let message = 'tickle',
+  font,
+  bounds, // 텍스트의 바운딩 박스에 대한 x, y, w, h값
+  fontsize = 60,
+  x,
+  y; // 텍스트의 x 와 y 좌표
+
+function preload() {
+  font = loadFont('assets/SourceSansPro-Regular.otf');
+}
 
 function setup() {
   createCanvas(710, 400);
-  strokeWeight(20.0);
-  stroke(255, 100);
-}
 
+  // 폰트 설정
+  textFont(font);
+  textSize(fontsize);
+
+  // 초기화시 중앙 정렬을 위해 텍스트의 너비 및 높이값 받아오기
+  bounds = font.textBounds(message, 0, 0, fontsize);
+  x = width / 2 - bounds.w / 2;
+  y = height / 2 - bounds.h / 2;
+}
 
 function draw() {
-  background("wheat");
+  background(204, 120);
 
-  dx = mouseX - x;
-  dy = mouseY - y;
-  angle1 = atan2(dy, dx);
-  x = mouseX - cos(angle1) * segLength;
-  y = mouseY - sin(angle1) * segLength;
+  // 텍스트를 검정색으로 쓰고, 그 바운딩 박스 받아오기
+  fill(0);
+  text(message, x, y);
+  bounds = font.textBounds(message, x, y, fontsize);
 
-  segment(x, y, angle1);
-  ellipse(x, y, 20, 20);
-}
-
-function segment(x, y, a) {
-  push();
-  translate(x, y);
-  rotate(a);
-  line(0, 0, segLength, 0);
-  pop();
+  // 마우스가 바운딩 박스 안에 있는지 확인하고, 안에 있다면 간질간질!
+  if (
+    mouseX >= bounds.x &&
+    mouseX <= bounds.x + bounds.w &&
+    mouseY >= bounds.y &&
+    mouseY <= bounds.y + bounds.h
+  ) {
+    x += random(-5, 5);
+    y += random(-5, 5);
+  }
 }
